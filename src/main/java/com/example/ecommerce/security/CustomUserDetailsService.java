19db2857d9fb298bfd1898d4f06bbcba1091ef0d
Yusuf Emre Bilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2024-08-10 20:43:03 +0300

    feat: add methods to fetch authenticated user info
    * updated CustomUserDetailsService

--- src/main/java/com/example/ecommerce/security/CustomUserDetailsService.java
+++ src/main/java/com/example/ecommerce/security/CustomUserDetailsService.java
@@ -10,18 +10,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    @Override
-    @Transactional
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+    }
 
-        return CustomUserDetails.build(user);
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return CustomUserDetails.build(getUserByUsername(username));
     }
 
 }
