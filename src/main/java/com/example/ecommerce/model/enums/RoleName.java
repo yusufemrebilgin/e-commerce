a2e107006607e87d988d76de005d22608605aa53
Yusuf Emre Bilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2024-08-29 23:32:57 +0300

    refactor: update RoleName enum

--- src/main/java/com/example/ecommerce/model/enums/RoleName.java
+++ src/main/java/com/example/ecommerce/model/enums/RoleName.java
@@ -1,7 +1,6 @@
 package com.example.ecommerce.model.enums;
 
 import java.util.HashSet;
-import java.util.Locale;
 import java.util.Set;
 
 public enum RoleName {
@@ -10,18 +9,21 @@ public enum RoleName {
     ROLE_ADMIN;
 
     public static RoleName fromName(String name) {
-        return switch (name) {
-            case "USER" -> ROLE_USER;
-            case "ADMIN" -> ROLE_ADMIN;
-            default -> throw new IllegalStateException("Invalid Role Name: " + name);
-        };
+        for (RoleName roleName : values()) {
+            String s = roleName.name().substring(5); // Remove ROLE_ prefix
+            if (s.equalsIgnoreCase(name)) {
+                return roleName;
+            }
+        }
+
+        throw new IllegalArgumentException("Invalid Role Name: " + name);
     }
 
     public static Set<RoleName> fromStrings(Set<String> rolesAsStringSet) {
         Set<RoleName> roleNames = new HashSet<>();
         if (rolesAsStringSet != null) {
             for (String role : rolesAsStringSet) {
-                roleNames.add(fromName(role.toUpperCase(Locale.ENGLISH)));
+                roleNames.add(fromName(role));
             }
         }
         return roleNames;
