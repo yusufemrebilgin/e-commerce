3fafac4a676f91f4cb5a1ce458de567f7ba7f4a1
Yusuf Emre Bilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2024-11-18 23:11:45 +0300

    chore: update ProductMapper and move InsufficientStockException

--- src/main/java/com/example/ecommerce/exception/product/InsufficientStockException.java
+++ src/main/java/com/example/ecommerce/exception/product/InsufficientStockException.java
@@ -1,12 +1,8 @@
-package com.example.ecommerce.exception.cart;
+package com.example.ecommerce.exception.product;
 
 import com.example.ecommerce.constant.ErrorMessages;
 
 public class InsufficientStockException extends RuntimeException {
-
-    public InsufficientStockException(String message) {
-        super(message);
-    }
 
     public InsufficientStockException(int availableStock, int requestedQuantity) {
         super(ErrorMessages.INSUFFICIENT_STOCK.message(availableStock, requestedQuantity));
--- src/main/java/com/example/ecommerce/mapper/ProductMapper.java
+++ src/main/java/com/example/ecommerce/mapper/ProductMapper.java
@@ -1,7 +1,9 @@
 package com.example.ecommerce.mapper;
 
+import com.example.ecommerce.model.embeddable.Discount;
 import com.example.ecommerce.model.Product;
 import com.example.ecommerce.model.ProductImage;
+import com.example.ecommerce.payload.request.product.UpdateProductRequest;
 import com.example.ecommerce.payload.response.ProductResponse;
 import lombok.NonNull;
 import org.springframework.stereotype.Component;
@@ -21,6 +23,26 @@ public class ProductMapper implements Mapper<Product, ProductResponse> {
                 product.getDiscount(),
                 product.getImages().stream().map(ProductImage::getUrl).toList()
         );
+    }
+
+    public void updateProductFromRequest(@NonNull UpdateProductRequest request, @NonNull Product existingProduct) {
+        existingProduct.setName(request.name());
+        existingProduct.setDescription(request.description());
+        existingProduct.setStock(request.stock());
+        existingProduct.setPrice(request.price());
+
+        Discount discount = existingProduct.getDiscount();
+        if (discount == null) {
+            discount = new Discount();
+            existingProduct.setDiscount(discount);
+        }
+
+        if (request.discountPercentage() != null)
+            discount.setPercentage(request.discountPercentage());
+        if (request.discountStart() != null)
+            discount.setStart(request.discountStart());
+        if (request.discountEnd() != null)
+            discount.setEnd(request.discountEnd());
     }
 
 }
