d1d81b4838979bb5b4881f0c6764b67e31f88e03
Yusuf Emre Bilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2024-11-18 23:14:23 +0300

    feat: add javadoc for payment related classes
    * update InvalidPaymentMethodException

--- src/main/java/com/example/ecommerce/service/PaymentService.java
+++ src/main/java/com/example/ecommerce/service/PaymentService.java
@@ -2,6 +2,7 @@ package com.example.ecommerce.service;
 
 import com.example.ecommerce.model.Order;
 import com.example.ecommerce.model.Payment;
+import com.example.ecommerce.model.enums.OrderStatus;
 import com.example.ecommerce.model.enums.PaymentMethod;
 import com.example.ecommerce.model.enums.PaymentStatus;
 import com.example.ecommerce.repository.PaymentRepository;
@@ -20,7 +21,19 @@ public class PaymentService {
 
     private final PaymentRepository paymentRepository;
 
+    /**
+     * Process payment for a given order with a specified amount and payment method.
+     * <p>Note: It simulates payment process. Payment is always considered successful
+     * for now.
+     *
+     * @param order         order for which payment is processed
+     * @param paymentAmount amount to be paid
+     * @param paymentMethod payment method to be used (e.g., DEBIT_CARD, CREDIT_CARD)
+     * @return true if payment is processed successfully
+     */
     protected boolean processPayment(Order order, BigDecimal paymentAmount, String paymentMethod) {
+
+        order.setOrderStatus(OrderStatus.PROCESSING);
 
         Payment payment = Payment.builder()
                 .order(order)
