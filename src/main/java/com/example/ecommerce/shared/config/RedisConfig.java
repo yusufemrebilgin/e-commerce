e0a5a0daf0f40352dcb143e83d74b4fc1884ae3e
Yusuf Emre Bilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2025-02-21 15:26:22 +0300

    refactor: remove manual connection factory and configure redis in yaml files
    * configured application.yaml and docker-compose

--- src/main/java/com/example/ecommerce/shared/config/RedisConfig.java
+++ src/main/java/com/example/ecommerce/shared/config/RedisConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -15,11 +14,6 @@ import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
-
-    @Bean
-    public RedisConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory();
-    }
 
     @Bean
     public StringRedisSerializer stringRedisSerializer() {
